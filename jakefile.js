f14cd610dd46f2114ef3b4e29e1d1effc598e60f
Eli Mydlarz <eli.mydlarz@gmail.com>
2014-07-01 17:03:37 +1000

    Setup lint options.

--- jakefile.js
+++ jakefile.js
@@ -1,7 +1,37 @@
+/* global desc, task, jake, fail, complete */
+
+"use strict";
+
+function lintOptions() {
+    var options = {
+        bitwise: true,
+        curly: false,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        regexp: true,
+        undef: true,
+        strict: true,
+        trailing: true,
+        node: true
+    };
+    return options;
+}
+
 task("default", ["lint"]);
 
 desc("Lint everything");
 task("lint", [], function() {
     var lint = require("./build/lint/lint_runner.js");
-    lint.validateFile("jakefile.js", {},{});
+    var files = new jake.FileList();
+
+    files.include("**/*.js");
+    files.exclude("node_modules");
+
+    lint.validateFileList(files.toArray(), lintOptions(), {});
 });
